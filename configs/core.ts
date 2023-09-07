9752fc5bc8c4ed1aab34182d1456a9750344d5e6
Cleo <cleo.debeau@gmail.com>
2023-09-07 16:15:08 -0400

    Add default configuration for all values
    - When using the overriding .dev.ts config files, not having default
      values means manually filling them out to avoid compile errors. This
      gets tiresome after a while
    - By providing default config values, we can avoid any issues of this
      nature. In future, we may decide to instead check that the dev config
      and production config *combined* have the full extent of the config
      values, but that is out-of-scope of this patch

--- configs/core.ts
+++ configs/core.ts
@@ -1,7 +1,9 @@
 import type core from './coreType';
 
 const config: core = {
-  Global_Commands: true  // Set this to false in development
+  Global_Commands: true, // Set this to false in development
+  Discord_Bot_Token: '',
+  MongoDB_URI: ''
 };
 
 export default config;
--- configs/snowflakeMap.ts
+++ configs/snowflakeMap.ts
@@ -1,6 +1,18 @@
 import type snowflakeMap from './snowflakeMapType';
 
 const config: snowflakeMap = {
+  Staff_Roles: [],
+  Sr_Staff_Roles: [],
+  Report_Notification_Roles: [],
+  Report_Banned_Roles: [],
+  Reports_Channels: [],
+  Sr_Notify_Channel: '',
+  Mod_Logs_Channels: {},
+  Verified_Roles: [],
+  Mature_Roles: [],
+  Image_Ban_Roles: [],
+  Support_Channel: '',
+  Mod_Editable_Roles: {}
 }
 
 export default config;
