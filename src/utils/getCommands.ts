56a69f06295585f305455ff60aae567b60f5e8bc
hyper-neutrino <alex.yj.liao@gmail.com>
2024-07-14 11:05:12 -0400

    fix type error

--- src/utils/getCommands.ts
+++ src/utils/getCommands.ts
@@ -1,8 +1,16 @@
-import { ContextMenuCommandBuilder, SlashCommandBuilder } from '@discordjs/builders';
+import {
+  ResponsiveContextMenuCommandBuilder,
+  ResponsiveSlashCommandBuilder,
+} from '@interactionHandling/commandBuilders.js';
 import hGetModulesInFolder from './getModulesInFolder.js';
 
 export default async function hGetCommands(directory: string) {
   // return only SlashCommandBuilders
-  return (await hGetModulesInFolder(directory)).map(array => array[1])
-    .filter(module => module instanceof SlashCommandBuilder || module instanceof ContextMenuCommandBuilder);
+  return (await hGetModulesInFolder(directory))
+    .map((array) => array[1])
+    .filter(
+      (module) =>
+        module instanceof ResponsiveSlashCommandBuilder ||
+                module instanceof ResponsiveContextMenuCommandBuilder
+    );
 }
