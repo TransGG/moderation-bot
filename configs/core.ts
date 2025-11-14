19d1708a66686b47139643dd5fbb637c0496ba26
hyperneutrino <me@hyperneutrino.xyz>
2025-11-14 17:27:29 -0500

    fix ban appeal links

--- configs/core.ts
+++ configs/core.ts
@@ -4,7 +4,8 @@ const config: core = {
   Global_Commands: true, // Set this to false in development
   Discord_Bot_Token: '',
   MongoDB_URI: '',
-  Alt_Account_Sheet_URL: '' // https://docs.google.com/spreadsheets/d/SHEET_URL_ID/export?format=csv&id=SHEET_URL_ID&gid=0
+  Alt_Account_Sheet_URL: '', // https://docs.google.com/spreadsheets/d/SHEET_URL_ID/export?format=csv&id=SHEET_URL_ID&gid=0
+  Ban_Appeal_Form_URL: null,
 };
 
 export default config;
--- configs/coreType.d.ts
+++ configs/coreType.d.ts
@@ -2,5 +2,6 @@ export default interface core {
   Discord_Bot_Token: string,
   MongoDB_URI: string,
   Global_Commands: boolean,
-  Alt_Account_Sheet_URL: string
+  Alt_Account_Sheet_URL: string,
+  Ban_Appeal_Form_URL: string | null,
 }
